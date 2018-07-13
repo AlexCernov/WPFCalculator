d35250af7491b44ac5f4b1e887694d278fb9429c
cosmin <roman.cosmin97@yahoo.com>
2018-07-13 12:17:57 +0200

    ..

---
+++ WPFCalculator/Operations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WPFCalculator
+{
+    class Operations
+    {
+        public static Complex Add (Complex a,Complex b)
+        {
+            return a + b;
+        }
+        public static Complex Substract(Complex a, Complex b)
+        {
+            return a - b;
+        }
+        public static Complex Multiply(Complex a, Complex b)
+        {
+            return a * b;
+        }
+        public static Complex Divide(Complex a, Complex b)
+        {
+            return a / b;
+        }
+    }
+}
