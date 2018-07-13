2136903b4b195fdccbfd7b777b804df49529bb5b
Alex Cernov <cernovalex1@gmail.com>
2018-07-13 11:53:39 +0300

    Revert "Added  Operation Class"
    This reverts commit 6bfd0d30db5d863b034a3e0517dce31a5a98a38c.

--- WPFCalculator/Operations.cs
+++ WPFCalculator/Operations.cs
@@ -3,27 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel;
 
 namespace WPFCalculator
 {
     class Operations
     {
-        public static Complex Add (Complex a,Complex b)
-        {
-            return a + b;
-        }
-        public static Complex Substract(Complex a, Complex b)
-        {
-            return a - b;
-        }
-        public static Complex Multiply(Complex a, Complex b)
-        {
-            return a * b;
-        }
-        public static Complex Divide(Complex a, Complex b)
-        {
-            return a / b;
-        }
     }
 }
