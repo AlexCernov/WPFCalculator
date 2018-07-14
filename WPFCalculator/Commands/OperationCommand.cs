f13acb7df75fa9a857a761287eab75fcbce70247
cosmin <roman.cosmin97@yahoo.com>
2018-07-14 18:46:22 +0200

    Update UI

---
+++ WPFCalculator/Commands/OperationCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace WPFCalculator.Commands
+{
+    internal class OperationCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+
+        public OperationCommand(Action<object> execute)
+        : this(execute, null)
+        {
+        }
+
+        public OperationCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+
+        #region ICommand Members
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameters)
+        {
+            return canExecute == null ? true : canExecute(parameters);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameters)
+        {
+            execute(parameters);
+        }
+
+        #endregion // ICommand Members
+    }
+}
+
--- WPFCalculator/ViewModels/ViewModel.cs
+++ WPFCalculator/ViewModels/ViewModel.cs
@@ -1,9 +1,13 @@
 ﻿
+using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using WPFCalculator.Commands;
 using WPFCalculator.Model;
 
-namespace WPFCalculator.ViewModel
+namespace WPFCalculator.ViewModels
 {
-    internal class ViewModel
+    internal class ViewModel : INotifyPropertyChanged
     {
         
         public ViewModel()
@@ -18,20 +22,24 @@ namespace WPFCalculator.ViewModel
         {
             get
             {
-                if(result==null)
-                {
-                    return "empty";
-                }
-                else
-                {
-                    return result;
-                }
+                return result;
             }
             set
             {
                 result = value;
+                OnPropertyChanged("Result");
             }
         }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
         public string Number1
         {
             get { return number1; }
@@ -43,30 +51,118 @@ namespace WPFCalculator.ViewModel
             set { number2 = value; }
         }
 
+        private ICommand addCommand;
+        private ICommand subtractCommand;
+        private ICommand multiplyCommand;
+        private ICommand divideCommand;
 
-        public void Add()
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand AddCommand
         {
+            get
+            {
+                if (addCommand == null)
+                {
+                    addCommand = new OperationCommand(
+                        param => this.Add(),
+                        param => this.CanAdd()
+                    );
+                }
+                return addCommand;
+            }
+        }
+
+
+        public ICommand SubtractCommand
+        {
+            get
+            {
+                if (subtractCommand == null)
+                {
+                    subtractCommand = new OperationCommand(
+                        param => this.Subtract(),
+                        param => this.CanMultiply()
+                    );
+                }
+                return subtractCommand;
+            }
+        }
+
+        public ICommand MultiplyCommand
+        {
+            get
+            {
+                if (multiplyCommand == null)
+                {
+                    multiplyCommand = new OperationCommand(
+                        param => this.Multiply(),
+                        param => this.CanDivide()
+                    );
+                }
+                return multiplyCommand;
+            }
+        }
+
+        public ICommand DivideCommand
+        {
+            get
+            {
+                if (divideCommand == null)
+                {
+                    divideCommand = new OperationCommand(
+                        param => this.Divide(),
+                        param => this.CanDivide()
+                    );
+                }
+                return divideCommand;
+            }
+        }
+
+        private bool CanAdd()
+        {
+            return true;
+        }
+
+        private bool CanSubtract()
+        {
+            return true;
+        }
+        private bool CanMultiply()
+        {
+            return true;
+        }
+        private bool CanDivide()
+        {
+            return true;
+        }
+
+        private void Add()
+        {
+            
             Complex complexNumber1 = Complex.ToComplex(Number1);
             Complex complexNumber2 = Complex.ToComplex(Number2);
             Result = Operations.Add(complexNumber1, complexNumber2).ToString();
         }
-        public void Subtract()
+        private void Subtract()
         {
             Complex complexNumber1 = Complex.ToComplex(Number1);
             Complex complexNumber2 = Complex.ToComplex(Number2);
             Result = Operations.Subtract(complexNumber1, complexNumber2).ToString();
         }
-        public void Multiply()
+        private void Multiply()
         {
             Complex complexNumber1 = Complex.ToComplex(Number1);
             Complex complexNumber2 = Complex.ToComplex(Number2);
             Result = Operations.Multiply(complexNumber1, complexNumber2).ToString();
         }
-        public void Divide()
+        private void Divide()
         {
             Complex complexNumber1 = Complex.ToComplex(Number1);
             Complex complexNumber2 = Complex.ToComplex(Number2);
             Result = Operations.Divide(complexNumber1, complexNumber2).ToString();
         }
+
+
     }
 }
